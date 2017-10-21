6c3d9724a0b5cadae880ca295c570fb1cbf7e3a6
Damanpreet <daman.4880@gmail.com>
2017-10-21 15:06:31 +0530

    02 - Using Express Middlewares

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (request, response) => {
-  response.send('Hello from Express!!')
+// Express middlewares
+// next() signals Express that it can jump
+// to next middleware or route handler
+app.use((request, response, next) => {
+  console.log(request.headers)
+  next()
 })
+
+// Adds extra property 'chance'
+app.use((request, response, next) => {
+  request.chance = Math.random()
+  next()
+})
+
+app.get('/', (request, response) => {
+  response.json({
+    chance: request.chance
+  })
+})
+
 
 app.listen(port, (err) => {
   if(err) {

e68089d08be6d77caff666cf2c43f5e79279f571
Damanpreet <daman.4880@gmail.com>
2017-10-21 15:36:09 +0530

    05 - Rendering HTML

--- app/index.js
+++ app/index.js
@@ -1,13 +1,35 @@
 // app/index.js
 
-const calc = require('./calc')
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
 
-const numbersToAdd = [
-  3,
-  4,
-  10,
-  2
-]
 
-const result = calc.sum(numbersToAdd)
-console.log(`The result is: ${result}`)
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+
+app.get('/', (request, response) => {
+  response.render('home', {
+    name: 'Daman'
+  })
+})
+
+// Error handler function should be the last
+// function added with app.use
+// It has next callback, it can be used to chain
+// multiple error handlers.
+app.use((err, request, response, next) => {
+  //handle Error
+  console.log(err)
+  response.status(500).send('Something broke!')
+})
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,36 +1,7 @@
 // index.js
 
-const express = require('express')
-const app = express()
+const app = require('./app')
 const port = 3000
-
-// Express middlewares
-// next() signals Express that it can jump
-// to next middleware or route handler
-app.use((request, response, next) => {
-  console.log(request.headers)
-  next()
-})
-
-// Adds extra property 'chance'
-app.use((request, response, next) => {
-  request.chance = Math.random()
-  next()
-})
-
-app.get('/', (request, response) => {
-  throw new Error('Error!!!')
-})
-
-// Error handler function should be the last
-// function added with app.use
-// It has next callback, it can be used to chain
-// multiple error handlers.
-app.use((err, request, response, next) => {
-  //handle Error
-  console.log(err)
-  response.status(500).send('Something broke!')
-})
 
 app.listen(port, (err) => {
   if(err) {
